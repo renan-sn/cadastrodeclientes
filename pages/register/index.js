b600b4e857db6718eaaac9d911d96655e529e6c4
primeiro commit <you@example.com>
2019-09-09 00:51:26 -0300

    Novas alteraçoes

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,28 +1,62 @@
 $(function () {
-   
+
+    const usuariosRegistrados = [];
+
     $('#repo-form').on('submit', function(event) {
-        event.preventDefault();
 
         let entradaDeEmail = document.getElementById('entrada-de-texto');
         let entradaDeSenha1 = document.getElementById('entrada-de-senha1');
         let entradaDeSenha2 = document.getElementById('entrada-de-senha2');
 
         /* Verifica se há campos vazios */
-        if (entradaDeEmail.value.length === 0 || entradaDeSenha1.value.length === 0 || entradaDeSenha2.value.length === 0) {
-            alert('Há campos que não foram inseridos!');
+        if (entradaDeEmail.value.length === 0 && entradaDeSenha1.value.length === 0 && entradaDeSenha2.value.length === 0) {
+            alert('Insira os dados nos campos correspondentes!');
+            event.preventDefault();
+            return;
+        }
+        /* email */
+        if (entradaDeEmail.value.length === 0) {
+            alert('Insira um email para continuar');
+            event.preventDefault();
+            return;
+        }
+        /* senha1 */
+        if (entradaDeSenha1.value.length === 0) {
+            alert('Insira uma senha para continuar');
+            event.preventDefault();
+            return;
+        }
+        /* senha2 */
+        if (entradaDeSenha2.value.length === 0) {
+            alert('É necessário confirmar a senha para continuar');
+            event.preventDefault();
             return;
         }
 
-        /* Verificar quantidade de caracteres digitados na senha */
+        /* Verificar quantidade de caracteres foram digitados na senha */
         if (entradaDeSenha1.value.length <= 7 || entradaDeSenha2.value.length <= 7) {
-            alert('A senha deve conter 8 caracteres');
+            alert('A senha deve conter de 8 a 12 caracteres');
+            event.preventDefault();
+            return;
+        }
+
+        if (entradaDeSenha1.value !== entradaDeSenha2.value) {
+            alert('As senhas não coincidem');
+            event.preventDefault();
             return;
         }
         
         
+        registrarUsuario(entradaDeEmail, entradaDeSenha1);
 
         //console.log(entradaDeEmail);
         //console.log(entradaDeSenha1);
     });
+
+    function registrarUsuario(entradaDeEmail, entradaDeSenha1) { 
+            usuariosRegistrados.push({ email: entradaDeEmail.value, senha: entradaDeSenha1.value });
+            console.log(usuariosRegistrados);
+            event.preventDefault();
+    }
     
 });
