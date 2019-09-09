cf9dfd785659a53047b2cb94056e4e357b4298b2
primeiro commit <you@example.com>
2019-09-09 01:01:35 -0300

    Nenhuma alteraçao

--- pages/register/index.js
+++ pages/register/index.js
@@ -10,53 +10,53 @@ $(function () {
 
         /* Verifica se há campos vazios */
         if (entradaDeEmail.value.length === 0 && entradaDeSenha1.value.length === 0 && entradaDeSenha2.value.length === 0) {
-            alert('Insira os dados nos campos correspondentes!');
             event.preventDefault();
+            alert('Insira os dados nos campos correspondentes!');
             return;
         }
         /* email */
         if (entradaDeEmail.value.length === 0) {
-            alert('Insira um email para continuar');
             event.preventDefault();
+            alert('Insira um email para continuar');
             return;
         }
         /* senha1 */
         if (entradaDeSenha1.value.length === 0) {
-            alert('Insira uma senha para continuar');
             event.preventDefault();
+            alert('Insira uma senha para continuar');
             return;
         }
         /* senha2 */
         if (entradaDeSenha2.value.length === 0) {
-            alert('É necessário confirmar a senha para continuar');
             event.preventDefault();
+            alert('É necessário confirmar a senha para continuar');
             return;
         }
 
         /* Verificar quantidade de caracteres foram digitados na senha */
         if (entradaDeSenha1.value.length <= 7 || entradaDeSenha2.value.length <= 7) {
-            alert('A senha deve conter de 8 a 12 caracteres');
             event.preventDefault();
+            alert('A senha deve conter de 8 a 12 caracteres');
             return;
         }
 
         if (entradaDeSenha1.value !== entradaDeSenha2.value) {
-            alert('As senhas não coincidem');
             event.preventDefault();
+            alert('As senhas não coincidem');
             return;
         }
         
-        
+        /* Função de registros */
         registrarUsuario(entradaDeEmail, entradaDeSenha1);
-
-        //console.log(entradaDeEmail);
-        //console.log(entradaDeSenha1);
     });
 
+    /* Registro de Usuário */
     function registrarUsuario(entradaDeEmail, entradaDeSenha1) { 
-            usuariosRegistrados.push({ email: entradaDeEmail.value, senha: entradaDeSenha1.value });
-            console.log(usuariosRegistrados);
-            event.preventDefault();
+        event.preventDefault();
+        usuariosRegistrados.push({ 
+            email: entradaDeEmail.value, 
+            senha: entradaDeSenha1.value 
+        });       
     }
     
 });
